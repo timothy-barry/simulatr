1b7483505d255bd96aa2b7253c0e0ec04dc817c2
Tim <tbarry2@andrew.cmu.edu>
2022-03-23 15:55:10 -0400

    add example

--- ex_code.R
+++ ex_code.R
@@ -9,9 +9,11 @@ n <- 1000
 fixed_params <- list(betas = c(1, 2, -1, 4),
                      design_matrix = matrix(c(runif(n, 0, 5), rbinom(n, 1, 0.5),
                                             rnorm(n), runif(n, -2, 2)), ncol = 4),
-                     B = 2, seed = 4, n_processors = 5)
+                     B = 1000, seed = 4, n_processors = 5)
 
 # 3. create the data generation function
+# This should return a list of length B of datasets when "loop" is FALSE;
+# otherwise, this should return a single dataset
 generate_data_f <- function(sigma, beta_0, betas, design_matrix, B) {
   l <- beta_0 + as.numeric(design_matrix %*% betas)
   n <- length(l)
@@ -23,12 +25,13 @@ generate_data_f <- function(sigma, beta_0, betas, design_matrix, B) {
 }
 
 generate_data_spec_f <- simulatr_function(f = generate_data_f,
-                                          arg_names = formalArgs(generate_data_f), loop = FALSE)
+                                          arg_names = formalArgs(generate_data_f))
 
 # 4. create the method functions
+
 # a. OLS regression
-# Note: y is the response vector of length n,
-# the output is a data frame with columns "parameter," "target," "value"
+# Note: the first argument is the simulated dataset
+# the output needs to be a data frame; ideally, with columns "parameter," "target," "value"
 ols_f <- function(y, design_matrix) {
   library(magrittr)
   fit_ols <- lm(y ~ design_matrix)
@@ -40,7 +43,7 @@ ols_f <- function(y, design_matrix) {
                                    levels = c("(Intercept)", "design_matrix1", "design_matrix2", "design_matrix3", "design_matrix4"),
                                    labels = c("intercept", "beta_1", "beta_2", "beta_3", "beta_4")))
 }
-ols_spec_f <- simulatr_function(f = ols_f, arg_names = "design_matrix", loop = TRUE)
+ols_spec_f <- simulatr_function(f = ols_f, arg_names = formalArgs(ols_f)[-1], loop = TRUE)
 
 # b. lasso regression
 lasso_f <- function(y, design_matrix) {
@@ -50,10 +53,9 @@ lasso_f <- function(y, design_matrix) {
   data.frame(parameter = c("intercept", "beta_1", "beta_2", "beta_3", "beta_4"),
              target = "estimate", value = beta_hat)
 }
-lasso_spec_f <- simulatr_function(f = lasso_f, arg_names = "design_matrix", loop = TRUE)
+lasso_spec_f <- simulatr_function(f = lasso_f, arg_names = formalArgs(lasso_f)[-1], loop = TRUE)
 method_list <- list(ols = ols_spec_f, lasso = lasso_spec_f)
 
 sim_spec <- simulatr_specifier(parameter_grid = p_grid, fixed_parameters = fixed_params,
                    generate_data_function = generate_data_spec_f, run_method_functions = method_list)
-
 check <- check_simulatr_specifier_object(simulatr_spec = sim_spec, B_in = 3, parallel = TRUE)
