1249646a1ca53fded3aa32366e610a953b569f77
Tim <tbarry2@andrew.cmu.edu>
2022-06-16 20:17:21 -0400

    delete nextflow helper files

--- example_code/run_simulatr_pipeline.sh
+++ example_code/run_simulatr_pipeline.sh
@@ -1,8 +1,7 @@
 source ~/.research_config
 
-nextflow $LOCAL_CODE_DIR"simulatr-pipeline/main.nf" \
+nextflow $LOCAL_CODE_DIR"simulatr-pipeline/main_2.nf" \
   --simulatr_specifier_fp $HOME"/simulatr_dir/sim_spec_obj.rds" \
   --B 10 \
   --result_dir $HOME"/simulatr_dir" \
-  --result_file_name "result.rds" \
-  -resume
+  --result_file_name "result.rds"
